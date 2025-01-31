7ee0bde09a996ea2a8de975e8a4fc456dbaadc48
JorgeIvanJH <jorgeivan.jaramillo.herrera@gmail.com>
2025-01-31 18:07:34 +0000

    2.2 progress

--- code/include/create_edge_list.h
+++ code/include/create_edge_list.h
@@ -5,15 +5,54 @@
 #include "unique.h"
 #include "sort_rows.h"
 
+using namespace std;
+using namespace Eigen;
+
 void create_edge_list(const Eigen::MatrixXi& F,
                       Eigen::MatrixXi& H,
                       Eigen::MatrixXi& E,
                       Eigen::VectorXi& boundEMask){
-  H.resize(0,2);  //halfedges
+    int num_faces = 5;//F.rows();
+  H.resize(3* num_faces, 2);  //halfedges
   E.resize(0,2);
   boundEMask.resize(0);
- 
   //TODO
+  // halfedges (H): size 3|F| × 2
+  // edges (E): size |E| × 2
+  for (int i = 0; i < num_faces; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+		H.row(3 * i + j) << F(i, j), F(i, (j + 1) % 3);
+      
+    }
+  }
+  Eigen::MatrixXi H_copy = H;
+  sort_rows(H_copy);
+
+
+
+  cout << "H: " << H << endl;
+  cout << "H_copy: " << H_copy << endl;
+
+   vector<int> uniqueIndices, counts, inverseIndices;
+
+   unique(H_copy,uniqueIndices, counts, inverseIndices);
+
+   for (int i = 0; i < uniqueIndices.size(); i++)
+   {
+	   cout << "uniqueIndices: " << uniqueIndices[i] << endl;
+   }
+
+  // E.resize(uniqueIndices.size(), 2);
+  // boundEMask.resize(H.rows(), 1);
+  // for (int i = 0; i < uniqueIndices.size(); i++)
+  // {
+  //   E.row(i) = H_copy.row(uniqueIndices[i]);
+  //   boundEMask(uniqueIndices[i]) = counts[i] == 1;
+  // }
+
+
 }
 
 
